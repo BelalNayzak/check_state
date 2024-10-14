8a4090fc0c9d27ce1c2c6d11f4a9360611f8e298
BelalNayzak <belal.nayzak@gmail.com>
2024-10-14 23:51:14 +0300

    fix: ui fixes

--- lib/default_loader.dart
+++ lib/default_loader.dart
@@ -5,6 +5,6 @@ class DefaultLoader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(child: Center(child: CupertinoActivityIndicator()));
+    return const Center(child: CupertinoActivityIndicator());
   }
 }
